259ff5995be610959ee75626b2cd584af973dbd7
Sachko-Sergey <sachkosergey11@gmail.com>
2020-01-11 19:42:48 +0300

    new styles adaptive

--- js/common.js
+++ js/common.js
@@ -17,12 +17,23 @@ $(document).ready(function () {
       infinite: true,
       dotsClass: 'gallery__slider-dots',
       prevArrow: false,
-      nextArrow: false
-
-      // autoplay: true,
-      // autoplaySpeed: 3000,
-      // prevArrow: '<button type="button" class="slick-prev header__arrow-prev"><img src="img/index_page/header/icons/prev.svg" alt=""></button>',
-      // nextArrow: '<button type="button" class="slick-next header__arrow-next"><img src="img/index_page/header/icons/next.svg" alt=""></button>'
+      nextArrow: false,
+      responsive: [
+         {
+            breakpoint: 999,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 1
+            }
+         },
+         {
+            breakpoint: 415,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         }
+      ]
    });
 
    $('.news-slider').slick({
@@ -37,33 +48,58 @@ $(document).ready(function () {
       nextArrow: '<button type="button" class="slick-next news-slider__arrow-next"><img src="img/index_page/news/icons/next.svg" alt=""></button>',
       responsive: [
          {
+            breakpoint: 1301,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 1
+            }
+         },
+         {
+            breakpoint: 1171,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+               prevArrow: false,
+               nextArrow: false
+            }
+         },
+         {
+            breakpoint: 616,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               prevArrow: false,
+               nextArrow: false
+            }
+         },
+         {
             breakpoint: 414,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              prevArrow: false,
-              nextArrow: false
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               prevArrow: false,
+               nextArrow: false
             }
-          },
+         },
          {
-           breakpoint: 360,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1,
-             prevArrow: false,
-             nextArrow: false
-           }
+            breakpoint: 360,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               prevArrow: false,
+               nextArrow: false
+            }
          }
-       ]
+      ]
    });
 
 
    $('.header-top__center-lang').styler();
    $('.footer-inner__right-bottom-lang').styler();
    $('.product-filter__brand').styler();
-   
 
-   
+
+
    $('.header__slider').on('afterChange', function (event, slick, currentSlide) {
       $("#cp").text(currentSlide + 1);
    });
